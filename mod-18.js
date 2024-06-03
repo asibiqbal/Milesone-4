6f6881618b1001fc53dc1cd8535d57496ad8b4d7
asibiqbal <asibiqbal3@gmail.com>
2024-06-04 00:24:39 +0600

    added 18.5

--- mod-18.js
+++ mod-18.js
@@ -14,6 +14,8 @@
 // 18-2 While Loop
 // use let variable in while loop
 
+
+
 // let num = 1;
 
 // while(num <= 5){
@@ -26,6 +28,8 @@
 
 // 18-3 
 // Sum...
+
+
 
 // let num = 1;  loop variable
 // let sum = 0;
@@ -45,6 +49,8 @@
 // for ( Decalare loop variable; Loop Condition; increment/decrement ){
 //  loop statement
 // }
+
+
 
 // for(let num = 1; num <= 10; num++){
 //     // console.log(num);
@@ -66,3 +72,115 @@
 //     console.log(i)
 // }
 
+// 18_5 Problem Solving With For Loop
+// alwase declare variable in outside of the loop. if u daclare var in the loop, when the loop returns in the condition, it restart from the first.
+
+// let sum = 0;
+
+// for(i = 11; i <= 20; i++){
+//     console.log("Serial" , i);
+//     sum = sum + i;
+//     console.log("Sum" , sum);
+// }
+
+// Decremental
+
+// for (i = 10; i >= 0; i--){
+//     console.log(i);
+// }
+// ctrl + c to stop loop in terminal
+
+
+// for(i= 20; i >= 11; i--){
+//     console.log(i);
+// } 
+
+
+// pr-1
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+// for(i = 1; i <= 60; i++){
+//     // console.log("I will invest at least 6 hrs every single day for next 60 days!" , i)
+// }
+
+// pr-2
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+// for (i = 61; i <= 100; i += 2){
+//     console.log("Odd Numbers" , i);
+// }
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+
+
+ */
+// for(i = 78; i <= 98; i++){
+//     // console.log('All Numbers' ,i);
+//     if(i % 2 === 0){
+//         console.log('Even Numbers' , i);
+//     }
+// }
+
+
+// pr-3 
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+// sum = 0;
+// for(i = 91; i <= 129; i += 2){
+//     // console.log("Even Numbers" , i);
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+/***
+
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+// sum = 0;
+// for(i = 51; i <= 85; i++){
+//     if(i % 2 === 0){
+//         // console.log(i);
+//     }
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+/*programming hero*/
+
+// pr-4 
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+
+/*programming hero*/
+// for(i = 9; i <= 90 ; i += 9){
+//     console.log(i);
+// }
--- notes.js
+++ notes.js
@@ -3,12 +3,28 @@
 // 18-1
 // 1. What is a loop.
 // 2. for loop, use a for loop.
-
+// 3. use let var for and while loop.
 // 18-2
 // 3. while loop. the structure of while loop.
 // 4. how to stop infinity value in while loop.
 // 18-3
 // 5. some practises from github programming_hero-1 - js loop.
+// 18-4
+// There is a three part of for loop
+
+// for ( Decalare loop variable; Loop Condition; increment/decrement ){
+//  loop statement
+// }
+
+// its good to name the var 'i';
+// some examples of for loop, odd and even, 
+// alwase declare variable in outside of the loop. if u daclare var in the loop, when the loop returns in the condition, it restart from the first.
+// create a for loop with sum.
+// Incremetal loop
+// Decremental loop
+// ctrl + c to stop loop in terminal
+
+
 
 
 
